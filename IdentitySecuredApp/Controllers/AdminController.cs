cad214fe2a1223b39e6f1a6cce63915720058630
sushmitha <sushmitha16031999@gmail.com>
2021-10-12 19:51:17 +0530

    intial commit

---
+++ IdentitySecuredApp/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using IdentitySecuredApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentitySecuredApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public IActionResult Login(UserModel user)
+        {
+            if (user.UserName=="Admin" && user.Password=="Admin@16")
+            {
+                return RedirectToAction("Index", "Laptops", new { uname = user.UserName });
+            }
+            else
+                ViewBag.msg = "Invalid input credentials...";
+            return View();
+        }
+    }
+}
---
+++ IdentitySecuredApp/Migrations/20211011163048_intialmigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Phase03Project.Migrations
+{
+    public partial class intialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
